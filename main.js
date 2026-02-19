d009c8388cecfbe7820b4d735ff3588b92c6c916
lhycpa77 <lhycpa77@gmail.com>
2026-02-19 10:23:46 +0000

    Change theme to blue

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.querySelector('.cta button').addEventListener('click', () => {
   alert('수강 신청 페이지로 이동합니다.');
 });
+
+document.getElementById('toggle-mode').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
